b67ad4f9687d846587bf2bc245a4e91f3bb2aab0
TheresaDF <tdafr@dtu.dk>
2025-10-03 10:16:59 +0200

    Added pflmf

---
+++ src/client/pflmf.py
@@ -0,0 +1,68 @@
+from src.client.fedper import FedPerClient
+
+
+import sys
+import torch
+
+class pFLMFClient(FedPerClient):
+    def __init__(self, **commons):
+        super().__init__(**commons)
+        self.v_steps = float(getattr(self.args.pflmf, "num_steps_v", 1))
+        self.u_steps = float(getattr(self.args.pflmf, "num_steps_u", 1))
+        self.lr_v = float(getattr(self.args.pflmf, "lr_v", 0.0001))
+        self.lr_u = float(getattr(self.args.pflmf, "lr_u", 0.001))
+
+def fit(self):
+    self.model.train()
+
+    # --- v-steps ---
+    for param_group in self.optimizer.param_groups:
+        param_group['lr'] = self.lr_v
+
+    for _ in range(int(self.v_steps)):
+        for x, y in self.trainloader:
+            if len(x) <= 1:
+                continue
+
+            x, y = x.to(self.device), y.to(self.device)
+            logit = self.model(x)
+            loss = self.criterion(logit, y)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+
+            # Handle classifier gradients
+            if hasattr(self.model, 'classifier') and isinstance(self.model.classifier, torch.nn.Module):
+                self.model.classifier.zero_grad()
+            else:
+                for name, param in self.model.named_parameters():
+                    if name in self.personal_params_name and param.grad is not None:
+                        param.grad.zero_()
+
+            self.optimizer.step()
+
+    # --- u-steps ---
+    for param_group in self.optimizer.param_groups:
+        param_group['lr'] = self.lr_u
+
+    for _ in range(int(self.u_steps)):
+        for x, y in self.trainloader:
+            if len(x) <= 1:
+                continue
+
+            x, y = x.to(self.device), y.to(self.device)
+            logit = self.model(x)
+            loss = self.criterion(logit, y)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+
+            # Handle classifier gradients
+            if hasattr(self.model, 'classifier') and isinstance(self.model.classifier, torch.nn.Module):
+                self.model.classifier.zero_grad()
+            else:
+                for name, param in self.model.named_parameters():
+                    if name in self.personal_params_name and param.grad is not None:
+                        param.grad.zero_()
+
+            self.optimizer.step()
---
+++ src/server/pflmf.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser, Namespace
+from copy import deepcopy
+
+from src.server.fedavg import FedAvgServer     #get_fedavg_argparser
+from src.client.pflmf import pFLMFClient
+
+
+
+class pFLMFServer(FedAvgServer):
+    algorithm_name = str = "pFLMF"
+    all_model_params_personalized = False
+    return_diff = False  # Sending parameter differences
+    client_cls = pFLMFClient
+
+
+    @staticmethod
+    def get_hyperparams(args_list=None) -> Namespace:
+        parser = ArgumentParser()
+        parser.add_argument("--num_steps_v", type=float, default=1, help="pFLMF's number of v steps")
+        parser.add_argument("--num_steps_u", type=float, default=1, help="pFLMF's number of u steps")
+        parser.add_argument("--lr_v", type=float, default=0.0001, help="pFLMF's learning rate of v")
+        parser.add_argument("--lr_u", type=float, default=0.001, help="pFLMF's learning rate of u")     
+
+        return parser.parse_args(args_list)
